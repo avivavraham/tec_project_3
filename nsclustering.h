fa924f45ee7882dcab0e6a5b6f6cf01e380d67ea
Yoni Romm <yonatan.romm@sap.com>
2022-03-13 23:27:23 +0200

    changed function to get the num rows as input

--- nsclustering.h
+++ nsclustering.h
@@ -5,14 +5,14 @@
 #ifndef TEC_PROJECT_3_NSCLUSTERING_H
 #define TEC_PROJECT_3_NSCLUSTERING_H
 
-double weight(double* data_point_1, double* data_point_2);
-double** weighted_matrix(double** data_points_array);
+double weight(double* data_point_1, double* data_point_2, int);
+double** weighted_matrix(double** data_points_array, int);
 double **allocate_array_2d(int r, int c);
-double **degree_matrix(double ** weighted_matrix);
-double sum_of_row(double *row_vector);
+double **degree_matrix(double ** weighted_matrix, int);
+double sum_of_row(double *row_vector, int);
 void print_matrix(double **matrix, int size_row, int size_column);
 void **pow_matrix(double ** matrix,int size_row, int size_column , double pow_, int diag_pow_optional);
-double **degree_matrix_normalized(double ** degree_matrix);
+double **degree_matrix_normalized(double ** degree_matrix, int);
 double **matrix_multiplication(double ** matrix1, int row1_size, int column1_size,
                                double ** matrix2, int row2_size, int column2_size);
 double **matrix_subtraction(double ** matrix1, double ** matrix2, int row_size, int column_size);

8287673dd02c95c378abea1a542277ee49311e0b
Yoni Romm <yonatan.romm@sap.com>
2022-03-23 00:32:15 +0200

    change shell script

---
+++ comp.sh
@@ -0,0 +1,12 @@
+#sudo chmod 755 'filename'
+#ssh -L 8080:yonatanromm@gate.tau.ac.il:22 yonatanromm@nova.cs.tau.ac.il   
+
+python3 setup.py build_ext --inplace
+#./spkmeans 3 lnorm input_1.txt
+#gcc -ansi -Wall -Wextra -Werror -pedantic-errors spkmeans.c -lm -o spkmeans
+#python3 kmeans_pp.py 3 100 0.01 test_data/input_1_db_1.txt test_data/input_1_db_2.txt
+#python3 kmeans_pp.py 7 0.01 test_data/input_2_db_1.txt test_data/input_2_db_2.txt
+#python3 kmeans_pp.py 15 0.01 test_data/input_3_db_1.txt test_data/input_3_db_2.txt
+#
+#rm -r build
+#rm mykmeanssp.cpython-39-darwin.so

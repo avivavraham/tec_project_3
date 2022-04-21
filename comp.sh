62cb2ad92a24168f0a657b254363113edf73756d
Yoni Romm <yonatan.romm@sap.com>
2022-04-21 20:18:32 +0300

    added tester

--- comp.sh
+++ comp.sh
@@ -16,8 +16,8 @@
 #./spkmeans jacobi /Users/I555060/University/software-project/tec_project_3/output2.txt &> output3.txt
 #cat output.txt
 #./spkmeans wam testfiles/spk_9.txt | diff - testfiles/outputs/c/wam/spk_9.txt
-#./yuval_tester.sh /Users/I555060/University/software-project/tec_project_3/testfile
+#./yuval_tester.sh /Users/I555060/University/software-project/tec_project_3/testfiles
 python3 setup.py build_ext --inplace &> /dev/null
 python3 setup.py install &> /dev/null
-#python3 spkmeans.py 0 spk /Users/I555060/University/software-project/tec_project_3/testfiles/spk_0.txt &> output.txt
-#python3 spkmeans.py 0 jacobi /Users/I555060/University/software-project/tec_project_3/tests/wordexample.txt &> output.txt
+#python3 spkmeans.py 0 spk /Users/I555060/University/software-project/tec_project_3/testfiles/spk_1.txt &> output.txt
+#python3 spkmeans.py 0 spk /Users/I555060/University/software-project/tec_project_3/tests/wordexample.txt &> output.txt
--- runtester2.sh
+++ runtester2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./tester2.sh "C:\Users\Yoni\PycharmProjects\tec_project_3\testfiles" "c" "yes" "yes" "no"
+./adamtester.sh "C:\Users\Yoni\PycharmProjects\tec_project_3\testfiles" "c" "yes" "yes" "no"

5557590876bf51fa7c56150e3d32d6e5bad8887a
Yoni Romm <yonatan.romm@sap.com>
2022-04-24 19:31:26 +0300

    update tester

--- runtester2.sh
+++ runtester2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./adamtester.sh "C:\Users\Yoni\PycharmProjects\tec_project_3\testfiles" "c" "yes" "yes" "no"
+./tester.sh "C:\Users\Yoni\PycharmProjects\tec_project_3\testfiles" "c" "yes" "yes" "no"
